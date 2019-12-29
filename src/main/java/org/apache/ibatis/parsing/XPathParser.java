317d0c9378b9075db2878ab846d19ee19d92479f
shan-kylin1997 <zhuoqilin@126.com>
2019-12-29 12:52:49 +0800

    feature:XpathParse解析模块学习

--- src/main/java/org/apache/ibatis/parsing/XPathParser.java
+++ src/main/java/org/apache/ibatis/parsing/XPathParser.java
@@ -41,15 +41,59 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 /**
+ * XPath解析，对java自带的Xpath进行更进一层次的封装
+ * 主要用于解析：mybatis-config.xml和 **Mapper.xml文件
+ *
  * @author Clinton Begin
  * @author Kazuki Shimizu
  */
 public class XPathParser {
 
+
+  /**
+   * XML Document对象
+   * XML被解析后 生成的org.w3c.dom.document对象
+   */
   private final Document document;
+
+
+  /**
+   * 是否校验XML，一般情况下默认为true
+   * 校验规则就是XML文档开始位置指定的DTD文件或者XSD文件
+   */
   private boolean validation;
+
+  /**
+   * XML实体解析器
+   * org.xml.sax.EntityResolver对象
+   * 默认情况下，对XML进行校验时会基于 XML 文档开始位置指定的 DTD 文件或 XSD 文件。
+   * 例如说，解析 mybatis-config.xml 配置文件时，会加载 http://mybatis.org/dtd/mybatis-3-config.dtd 这个 DTD 文件。
+   * 但是，如果每个应用启动都从网络加载该 DTD 文件，势必在弱网络下体验非常下，甚至说应用部署在无网络的环境下，
+   * 还会导致下载不下来，那么就会出现 XML 校验失败的情况。
+   * 所以，在实际场景下，MyBatis 自定义了 EntityResolver 的实现，达到使用本地 DTD 文件，从而避免下载网络 DTD 文件的效果
+   * todo 说明真正的解析不在这里，这里只是一个更高层次的封装?
+   */
   private EntityResolver entityResolver;
+
+  /**
+   * 变量Properties对象 用来替换需要动态配置的属性值
+   * <dataSource type="POOLED">
+   *   <property name="driver" value="${driver}"/>
+   *   <property name="url" value="${url}"/>
+   *   <property name="username" value="dev_user"/>
+   *   <property name="password" value="${password}"/>
+   * </dataSource>
+   * 这里就可以替换${username}的值 而不是硬编码写在配置文件中---不安全
+   * variables来源：可以是写在配置文件中的硬编码（上面的dev_user），也可以是动态属性${password}获取
+   * 动态属性的获取：参考PropertyParser#parse(String string, Properties variables)
+   */
   private Properties variables;
+
+  /**
+   * Java java.xml.xpath.Xpath对象
+   * 用于查询XML中的节点和元素
+   * 对于Xpath不理解的请翻阅 org.apach.ibatis.pasring.XpathLearn
+   */
   private XPath xpath;
 
   public XPathParser(String xml) {
@@ -112,6 +156,14 @@ public class XPathParser {
     this.document = document;
   }
 
+  /**
+   * 构造器
+   * @param xml xml文件实体
+   * @param validation 是否校验
+   * @param variables 参数对象
+   * @param entityResolver 解析器实体
+   * @return XPathParser
+   */
   public XPathParser(String xml, boolean validation, Properties variables, EntityResolver entityResolver) {
     commonConstructor(validation, variables, entityResolver);
     this.document = createDocument(new InputSource(new StringReader(xml)));
@@ -219,29 +271,49 @@ public class XPathParser {
     return new XNode(this, node, variables);
   }
 
+
+  /**
+   * 获取指定元素或节点的值
+   * @param expression
+   * @param root
+   * @param returnType
+   * @return Object
+   */
   private Object evaluate(String expression, Object root, QName returnType) {
     try {
+      //获得指定元素或节点的值
       return xpath.evaluate(expression, root, returnType);
     } catch (Exception e) {
       throw new BuilderException("Error evaluating XPath.  Cause: " + e, e);
     }
   }
 
+  /**
+   * 创建Document对象---将XML实体转换成Document
+   * @param inputSource
+   * @return Document
+   */
   private Document createDocument(InputSource inputSource) {
     // important: this must only be called AFTER common constructor
+    //重要：这个必须在 common constructor后调用
     try {
+      //1.构建DocumentBuilderFactory对象 以及一些基本的配置
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
       factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+      //是否对XML进行检验 使用的是成员属性的值
       factory.setValidating(validation);
-
+      //工厂的基本配置 无关核心内容
       factory.setNamespaceAware(false);
       factory.setIgnoringComments(true);
       factory.setIgnoringElementContentWhitespace(false);
       factory.setCoalescing(false);
       factory.setExpandEntityReferences(true);
 
+      //2.构建DocumentBuilder对象
       DocumentBuilder builder = factory.newDocumentBuilder();
+      // 设置解析器实体 使用成员属性的值
       builder.setEntityResolver(entityResolver);
+      //设置异常处理器 发生异常后的处理  无关核心
       builder.setErrorHandler(new ErrorHandler() {
         @Override
         public void error(SAXParseException exception) throws SAXException {
@@ -258,16 +330,24 @@ public class XPathParser {
           // NOP
         }
       });
+      //3.创建Document对象
       return builder.parse(inputSource);
     } catch (Exception e) {
       throw new BuilderException("Error creating document instance.  Cause: " + e, e);
     }
   }
 
+  /**
+   * 通用构造器--16个构造器都需要用到的，抽离出来公用
+   * @param validation
+   * @param variables
+   * @param entityResolver
+   */
   private void commonConstructor(boolean validation, Properties variables, EntityResolver entityResolver) {
     this.validation = validation;
     this.entityResolver = entityResolver;
     this.variables = variables;
+    //构建Xpath对象
     XPathFactory factory = XPathFactory.newInstance();
     this.xpath = factory.newXPath();
   }
---
+++ src/main/java/org/apache/ibatis/parsing/XpathLearn.java
@@ -0,0 +1,130 @@
+package org.apache.ibatis.parsing;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+/**
+ * Xpath学习
+ * XPath主要用于解析XML文档
+ * @author kylin
+ * Date: 2019/12/29 11:50
+ **/
+public class XpathLearn {
+
+
+  /**
+   * 模拟xml文件读取
+   * @return String
+   */
+  public static String produceXmlFile() {
+
+    return "<class>\n" +
+      "   <student rollno=\"393\">\n" +
+      "      <firstname>dinkar</firstname>\n" +
+      "      <lastname>kad</lastname>\n" +
+      "      <nickname>dinkar</nickname>\n" +
+      "      <marks>85</marks>\n" +
+      "   </student>\n" +
+      "   <student rollno=\"493\">\n" +
+      "      <firstname>Vaneet</firstname>\n" +
+      "      <lastname>Gupta</lastname>\n" +
+      "      <nickname>vinni</nickname>\n" +
+      "      <marks>95</marks>\n" +
+      "   </student>\n" +
+      "   <student rollno=\"593\">\n" +
+      "      <firstname>jasvir</firstname>\n" +
+      "      <lastname>singh</lastname>\n" +
+      "      <nickname>jazz</nickname>\n" +
+      "      <marks>90</marks>\n" +
+      "   </student>\n" +
+      "</class>\n";
+
+
+//    return "<?xml version=\"1.0\"?>\n" +
+//      "<class>\n" +
+//      "   <student rollno=\"393\">\n" +
+//      "      <firstname>dinkar</firstname>\n" +
+//      "      <lastname>kad</lastname>\n" +
+//      "      <nickname>dinkar</nickname>\n" +
+//      "      <marks>85</marks>\n" +
+//      "   </student>\n" +
+//      "   <student rollno=\"493\">\n" +
+//      "      <firstname>Vaneet</firstname>\n" +
+//      "      <lastname>Gupta</lastname>\n" +
+//      "      <nickname>vinni</nickname>\n" +
+//      "      <marks>95</marks>\n" +
+//      "   </student>\n" +
+//      "   <student rollno=\"593\">\n" +
+//      "      <firstname>jasvir</firstname>\n" +
+//      "      <lastname>singh</lastname>\n" +
+//      "      <nickname>jazz</nickname>\n" +
+//      "      <marks>90</marks>\n" +
+//      "   </student>\n" +
+//      "</class>\n";
+
+  }
+
+  /**
+   * 执行顺序
+   * 1.获取xml配置文件
+   * 2.解析成Document对象
+   * 3.xpath解析===路径表达式解析元素
+   * 4.获取到解析到的element对象，进行value获取
+   */
+  public static void main(String []args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
+    String xmlEntity = produceXmlFile();
+    //1.构建Document对象
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+    Document document = documentBuilder.parse(new ByteArrayInputStream(xmlEntity.getBytes()));
+
+    //2.构建Xpath对象 并且使用解析路径解析元素
+    XPath xPath = XPathFactory.newInstance().newXPath();
+    //解析class标签下面的student标签【这个应该是xml文件编写的时候约定的，或者说使用正则表达式】
+    String parseExpression = "/class/student";
+    NodeList nodeList = (NodeList) xPath.compile(parseExpression).evaluate(document, XPathConstants.NODESET);
+
+    //3.获取到Node节点后 遍历获取Element元素以及内部的值
+    for (int i = 0; i < nodeList.getLength(); i++) {
+      Node node = nodeList.item(i);
+      System.out.println("当前节点名：" + node.getNodeName());
+      //如果当前节点是元素节点 Node和Element应该是父子包含关系 节点有多种类型：其中就有元素节点Element
+      if (node.getNodeType() == Node.ELEMENT_NODE) {
+        Element element = (Element) node;
+        //节点自身的attribute
+        System.out.println("学生roll no：" + element.getAttribute("rollno"));
+
+        //节点之下的子节点
+        System.out.println("=======子节点部分======");
+        System.out.println("firstname:"
+          + element.getElementsByTagName("firstname").item(0).getTextContent());
+        System.out.println("lastname:"
+          + element.getElementsByTagName("lastname").item(0).getTextContent());
+        System.out.println("nickname:"
+          + element.getElementsByTagName("nickname").item(0).getTextContent());
+        System.out.println("marks:"
+          + element.getElementsByTagName("marks").item(0).getTextContent());
+      }
+
+    }
+  }
+
+
+
+
+
+
+}
